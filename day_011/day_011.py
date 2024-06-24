5290c43af80b5d6e3699d44b459932ab8d4cab40
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-06-24 07:59:55 -0300

    feat: day #011 complete, add python file

---
+++ day_011/day_011.py
@@ -0,0 +1,8 @@
+lista = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+print(lista[0])  # Saída: [1, 2, 3]
+print(lista[1][0])  # Saída: 4
+
+lista = [i for i in range(10)]
+
+print(lista)  # Saída: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
