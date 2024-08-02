0db542d88dae5f4c5ef9440bf34ede4a0f22e656
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-08-01 21:54:15 -0300

    feat: day #049 complete, add python file

---
+++ day_049/day_049.py
@@ -0,0 +1,17 @@
+import cmath
+
+cmath_number = 1 + 2j
+
+print(cmath_number)
+
+print(cmath_number.real)
+
+print(cmath_number.imag)
+
+print(cmath_number.conjugate())
+
+print(abs(cmath_number))
+
+print(cmath.phase(cmath_number))
+
+print(cmath.rect(2, cmath.pi / 2))
