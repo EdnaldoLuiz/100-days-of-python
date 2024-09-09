b62469f81d7983459c98d3f0c7f1a9ba6435bc56
Ednaldo Luiz <ednaldo.eln68@gmail.com>
2024-09-08 22:31:42 -0300

    feat: day #087 complete, add python file

---
+++ day_087/day_087.py
@@ -0,0 +1,29 @@
+from http.server import SimpleHTTPRequestHandler, HTTPServer
+import urllib.parse as urlparse
+
+class MyHandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(b"<html><body><h1>GET recebido!</h1></body></html>")
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        parsed_data = urlparse.parse_qs(post_data.decode('utf-8'))
+        response = f"<html><body><h1>POST recebido!</h1><p>Data: {parsed_data}</p></body></html>"
+        
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+        self.wfile.write(response.encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=MyHandler, port=8080):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Startando servidor na porta {port}...')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    run()
